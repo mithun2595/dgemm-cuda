ee62af57f02c5155777bd5762559394d3d1eab39
Sai Kishan Pampana <spampana@eng.ucsd.edu>
2017-10-30 16:24:59 -0700

    Implemented tiling but did not tile the parameter k

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -7,16 +7,39 @@ using namespace std;
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
-    int I =  blockIdx.y*blockDim.y + threadIdx.y;
-    int J =  blockIdx.x*blockDim.x + threadIdx.x;
+    // int I =  blockIdx.y*blockDim.y + threadIdx.y;
+    // int J =  blockIdx.x*blockDim.x + threadIdx.x;
+    //
+    // if((I < N) && (J < N)){
+    //     _DOUBLE_ _c = 0;
+    //     for (unsigned int k = 0; k < N; k++) {
+    //         _DOUBLE_ a = A[I * N + k];
+    //         _DOUBLE_ b = B[k * N + J];
+    //         _c += a * b;
+    //     }
+    //     C[I * N + J] = _c;
+    // }
 
-    if((I < N) && (J < N)){
-        _DOUBLE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++) {
-            _DOUBLE_ a = A[I * N + k];
-            _DOUBLE_ b = B[k * N + J];
-            _c += a * b;
+    __shared__ double As, Bs;
+
+    int i = threadIdx.y;
+    int j = threadIdx.x;
+    int ii = blockIdx.y;
+    int jj = blockIdx.x;
+
+    int TI = 16, TJ = 16, TK = 32;
+
+    _DOUBLE_ Cij = 0;
+    if((ii*16 + i < N) && (jj*16 + j < N))
+    {
+      // for(int kk = 0; kk < N; kk+= TK)
+      // {
+        for(int k =0; k < N; k++)
+        {
+          Cij += A[(ii*TI+i)*N + k]*B[k*N + jj*TJ + j];
         }
-        C[I * N + J] = _c;
+      // }
+
+      C[(ii*TI+i)*N + jj*TJ + j] = Cij;
     }
 }
