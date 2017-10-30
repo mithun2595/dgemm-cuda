ac6125b724774cc0db642894bcfce1ab87a60559
Sai Kishan Pampana <spampana@eng.ucsd.edu>
2017-10-30 16:23:55 -0700

    Updated to include the sizes in the code

--- setGrid.cu
+++ setGrid.cu
@@ -1,6 +1,8 @@
 void setGrid(int n, dim3 &blockDim, dim3 &gridDim)
 {
    // set your block dimensions and grid dimensions here
+   blockDim.x = 16;
+   blockDim.y = 16;
    gridDim.x = n / blockDim.x;
    gridDim.y = n / blockDim.y;
    if(n % blockDim.x != 0)
