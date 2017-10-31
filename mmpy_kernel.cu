6c1349868456b65e812fc700b577ee2c46ccf10b
Sai Kishan Pampana <spampana@eng.ucsd.edu>
2017-10-31 15:56:07 -0700

    Implemented the shared memory code give in the lecture slides

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -9,37 +9,57 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
     // int I =  blockIdx.y*blockDim.y + threadIdx.y;
     // int J =  blockIdx.x*blockDim.x + threadIdx.x;
-    //
+
     // if((I < N) && (J < N)){
-    //     _DOUBLE_ _c = 0;
-    //     for (unsigned int k = 0; k < N; k++) {
-    //         _DOUBLE_ a = A[I * N + k];
-    //         _DOUBLE_ b = B[k * N + J];
-    //         _c += a * b;
-    //     }
-    //     C[I * N + J] = _c;
+//        _DOUBLE_ _c = 0;
+//        for (unsigned int k = 0; k < N; k++) {
+//            _DOUBLE_ a = A[I * N + k];
+//            _DOUBLE_ b = B[k * N + J];
+//            _c += a * b;
+//        }
+//        C[I * N + J] = _c;
+    // }
+    const int TW = 16;
+
+    __shared__ _DOUBLE_ As[TW][TW], Bs[TW][TW];
+     int ty = threadIdx.y, tx = threadIdx.x;
+     int by = blockIdx.y, bx = blockIdx.x;
+     int I = by*TW + ty, J = bx*TW + tx;
+    //  int i = threadIdx.y;
+    //  int j = threadIdx.x;
+    //  int ii = blockIdx.y;
+    //  int jj = blockIdx.x;
+
+    //  int TI = 16, TJ = 16, TK = 32;
+
+     _DOUBLE_ Cij = 0;
+    // if((ii*TI + i < N) && (jj*TJ + j < N))
+    // {
+       // for(int kk = 0; kk < N; kk+= TK)
+       // {
+        //  for(int k =0; k < N; k++)
+        //  {
+        //    Cij += A[(ii*TI+i)*N + k]*B[k*N + jj*TJ + j];
+        //  }
+       // }
+
+      //  C[(ii*TI+i)*N + jj*TJ + j] = Cij;
     // }
 
-    __shared__ double As, Bs;
-
-    int i = threadIdx.y;
-    int j = threadIdx.x;
-    int ii = blockIdx.y;
-    int jj = blockIdx.x;
-
-    int TI = 16, TJ = 16, TK = 32;
-
-    _DOUBLE_ Cij = 0;
-    if((ii*16 + i < N) && (jj*16 + j < N))
+    for(int kk = 0; kk < N/TW; kk++)
     {
-      // for(int kk = 0; kk < N; kk+= TK)
-      // {
-        for(int k =0; k < N; k++)
-        {
-          Cij += A[(ii*TI+i)*N + k]*B[k*N + jj*TJ + j];
-        }
-      // }
+      As[ty][tx] = A[I*N+kk*TW+tx];
+      Bs[ty][tx] = B[(kk*TW+ty)*N+J];
+      __syncthreads();
 
-      C[(ii*TI+i)*N + jj*TJ + j] = Cij;
+      for(int k = 0; k < TW; k++)
+      {
+        Cij += As[ty][k]*Bs[k][tx];
+      }
+      __syncthreads();
     }
+    C[I*N+J] = Cij;
+
+
+
 }
