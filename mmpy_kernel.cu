36709f278009fa9ddebbb432c75f5869229a5599
mdharmar <mdharmar@eng.ucsd.edu>
2017-11-03 21:24:42 -0700

    restructured loads

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -9,7 +9,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
       const unsigned int TW = 32;
       const unsigned int TW1 = TW/4, TW2 = TW/2, TW3 = 3*TW/4;
-      const unsigned int edge_limit = (int) ceilf((float)N/TW);
+      const unsigned int edge_limit = N/TW;
       const unsigned int ty = threadIdx.y, tx = threadIdx.x;
       const unsigned int by = blockIdx.y, bx = blockIdx.x;
       const unsigned int I = by*TW + ty, J = bx*TW + tx;
@@ -17,80 +17,54 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
      __shared__ _DOUBLE_ As[TW][TW], Bs[TW][TW];
 
      _DOUBLE_ Cs[4] = {0};
+     As[ty][tx] = 0;
+     As[ty+TW1][tx] = 0;
+     As[ty+TW2][tx] = 0;
+     As[ty+TW3][tx] = 0;
+
+     Bs[ty][tx] = 0;
+     Bs[ty+TW1][tx] = 0;
+     Bs[ty+TW2][tx] = 0;
+     Bs[ty+TW3][tx] = 0;
+
+     if(N%TW !=0)
+     {
+       for(int Ai =I; (Ai < N) && (Ai < I + TW); Ai +=TW/4)
+       {
+         if(edge_limit*TW+tx < N)
+          As[ty+Ai-I][tx] = A[(Ai)*N+(edge_limit*TW+tx)];
+       }
+       int b_start = edge_limit*TW+ty;
+       for(int Bi = b_start;(Bi<N) && (Bi < b_start+TW) ; Bi+=TW/4)
+       {
+        if(J < N)
+          Bs[Bi-edge_limit*TW][tx] = B[Bi*N+J];
+
+       }
+       __syncthreads();
+
+       for(int k = 0; k < TW; k++)
+       {
+         Cs[0] += As[ty][k] * Bs[k][tx];
+         Cs[1] += As[ty+TW1][k] * Bs[k][tx];
+         Cs[2] += As[ty+TW2][k] * Bs[k][tx];
+         Cs[3] += As[ty+TW3][k] * Bs[k][tx];
+       }
+
+     }
+      __syncthreads();
 
     for(int kk = 0; kk < edge_limit; kk++)
     {
 
-      if((kk*TW+tx)<N)
-      {
-        if(I+TW3 < N) {
-          As[ty][tx] = A[I*N+(kk*TW+tx)];
-          As[ty+TW1][tx] = A[(I+TW1)*N+(kk*TW+tx)];
-          As[ty+TW2][tx] = A[(I+TW2)*N+(kk*TW+tx)];
-          As[ty+TW3][tx] = A[(I+TW3)*N+(kk*TW+tx)];
-        } else if(I+TW2 < N){
-          As[ty][tx] = A[I*N+(kk*TW+tx)];
-          As[ty+TW1][tx] = A[(I+TW1)*N+(kk*TW+tx)];
-          As[ty+TW2][tx] = A[(I+TW2)*N+(kk*TW+tx)];
-          As[ty+TW3][tx] = 0;
-        } else if(I+TW1 < N) {
-          As[ty][tx] = A[I*N+(kk*TW+tx)];
-          As[ty+TW1][tx] = A[(I+TW1)*N+(kk*TW+tx)];
-          As[ty+TW2][tx] = 0;
-          As[ty+TW3][tx] = 0;
-        } else if(I < N) {
-          As[ty][tx] = A[I*N+(kk*TW+tx)];
-          As[ty+TW1][tx] = 0;
-          As[ty+TW2][tx] = 0;
-          As[ty+TW3][tx] = 0;
-        } else {
-          As[ty][tx] = 0;
-          As[ty+TW1][tx] = 0;
-          As[ty+TW2][tx] = 0;
-          As[ty+TW3][tx] = 0;
-        }
-
-      } else {
-        As[ty][tx] = 0;
-        As[ty+TW1][tx] = 0;
-        As[ty+TW2][tx] = 0;
-        As[ty+TW3][tx] = 0;
-      }
-
-      if(J<N)
-      {
-        if(kk*TW+ty+TW3 < N) {
-          Bs[ty][tx] = B[(kk*TW+ty)*N+J];
-          Bs[ty+TW1][tx] = B[(kk*TW+ty+TW1)*N+J];
-          Bs[ty+TW2][tx] = B[(kk*TW+ty+TW2)*N+J];
-          Bs[ty+TW3][tx] = B[(kk*TW+ty+TW3)*N+J];
-        } else if(kk*TW+ty+TW2 < N){
-          Bs[ty][tx] = B[(kk*TW+ty)*N+J];
-          Bs[ty+TW1][tx] = B[(kk*TW+ty+TW1)*N+J];
-          Bs[ty+TW2][tx] = B[(kk*TW+ty+TW2)*N+J];
-          Bs[ty+TW3][tx] = 0;
-        } else if(kk*TW+ty+TW1 < N) {
-          Bs[ty][tx] = B[(kk*TW+ty)*N+J];
-          Bs[ty+TW1][tx] = B[(kk*TW+ty+TW1)*N+J];
-          Bs[ty+TW2][tx] = 0;
-          Bs[ty+TW3][tx] = 0;
-        } else if(kk*TW+ty < N) {
-          Bs[ty][tx] = B[(kk*TW+ty)*N+J];
-          Bs[ty+TW1][tx] = 0;
-          Bs[ty+TW2][tx] = 0;
-          Bs[ty+TW3][tx] = 0;
-        } else {
-          Bs[ty][tx] = 0;
-          Bs[ty+TW1][tx] = 0;
-          Bs[ty+TW2][tx] = 0;
-          Bs[ty+TW3][tx] = 0;
-        }
-      } else {
-        Bs[ty][tx] = 0;
-        Bs[ty+TW1][tx] = 0;
-        Bs[ty+TW2][tx] = 0;
-        Bs[ty+TW3][tx] = 0;
-      }
+      As[ty][tx] = A[I*N+(kk*TW+tx)];
+      As[ty+TW1][tx] = A[(I+TW1)*N+(kk*TW+tx)];
+      As[ty+TW2][tx] = A[(I+TW2)*N+(kk*TW+tx)];
+      As[ty+TW3][tx] = A[(I+TW3)*N+(kk*TW+tx)];
+      Bs[ty][tx] = B[(kk*TW+ty)*N+J];
+      Bs[ty+TW1][tx] = B[(kk*TW+ty+TW1)*N+J];
+      Bs[ty+TW2][tx] = B[(kk*TW+ty+TW2)*N+J];
+      Bs[ty+TW3][tx] = B[(kk*TW+ty+TW3)*N+J];
       __syncthreads();
 
       #pragma unroll
