e9efc2f223df3f4079461cbec444d46112301b56
Mithun <mithun2595@gmail.com>
2017-11-10 23:59:20 -0800

    FINAL SUBMISSION

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -8,18 +8,19 @@ using namespace std;
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
       register const unsigned int TW = 32;
-      register const unsigned int Tw[8] = {0,TW/8, TW/4, 3*TW/8, TW/2, 5*TW/8, 3*TW/4, 7*TW/8};
+      register const unsigned int Tw[8] = {0,TW/8, TW/4, 3*TW/8, TW/2, 5*TW/8, 3*TW/4, 7*TW/8}; // Array used to do unrolling.
       const unsigned int edge_limit = N/TW;
       register const unsigned int ty = threadIdx.y, tx = threadIdx.x;
       register const unsigned int by = blockIdx.y, bx = blockIdx.x;
       register const unsigned int I = by*TW + ty, J = bx*TW + tx;
 
-      __shared__ _DOUBLE_ As[TW][TW], Bs[TW][TW];
+      __shared__ _DOUBLE_ As[TW][TW], Bs[TW][TW]; // Shared memory used to store the tiles of A and B
 
-     register _DOUBLE_ Cs[8] = {0};
+     register _DOUBLE_ Cs[8] = {0}; // Array used to store the computed value of C.
      register int i = 0;
      register int kk = 0;
 
+     // This section of the code handles the initialising of the As and Bs to 0.
     #pragma unroll
     for(i = 0; i < 8; i++)
     {
@@ -31,6 +32,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
         Bs[ty+Tw[i]][tx] = 0;
      }
 
+     // This section of the code handles the corner cases.
      if(N%TW !=0)
      {
        register int a_1 = ty - I;
@@ -50,6 +52,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
        }
        __syncthreads();
 
+       // This section handles the computation of Cs
        for(int k = 0; k < TW; k++)
        {
          #pragma unroll
@@ -62,9 +65,10 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
      }
       __syncthreads();
 
+      // This seciton of the code is for the perfect case without corner cases.
     for( kk = 0; kk < edge_limit; kk++)
     {
-
+      // Loading of A into As and B into Bs
       #pragma unroll
       for(i = 0; i < 8; i++)
       {
@@ -80,7 +84,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
       __syncthreads();
 
-
+      // Computing the values of C
       for(int k = 0; k < TW; k++)
       {
         #pragma unroll
@@ -92,7 +96,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
       }
       __syncthreads();
     }
-
+    //Storing the values of Cs to C while checking the indices within range
      if((I<N)&&(J<N))
      {
        C[I*N+J] = Cs[0];
