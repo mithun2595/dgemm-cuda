065f52768d8fb75a2a7116c159ddb50c814ef3c7
Sai Kishan Pampana <spampana@eng.ucsd.edu>
2017-11-10 23:38:00 -0800

    Commented the code

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -8,26 +8,18 @@ using namespace std;
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
       register const unsigned int TW = 32;
-      register const unsigned int Tw[8] = {0,TW/8, TW/4, 3*TW/8, TW/2, 5*TW/8, 3*TW/4, 7*TW/8};
-      const unsigned int edge_limit = N/TW;
+      register const unsigned int Tw[8] = {0,TW/8, TW/4, 3*TW/8, TW/2, 5*TW/8, 3*TW/4, 7*TW/8}; // This array helps us in the unrolling by 8.
+      const unsigned int edge_limit = N/TW; // This represent the number of thread blocks that are not in the corner cases. 
       const unsigned int ty = threadIdx.y, tx = threadIdx.x;
       const unsigned int by = blockIdx.y, bx = blockIdx.x;
       const unsigned int I = by*TW + ty, J = bx*TW + tx;
 
-      __shared__ _DOUBLE_ As[TW][TW], Bs[TW][TW];
+      __shared__ _DOUBLE_ As[TW][TW], Bs[TW][TW]; // Shared memory that is used to stored the tiles of A and B for computation. 
 
-     register _DOUBLE_ Cs[8] = {0};
+     register _DOUBLE_ Cs[8] = {0}; // This array of Cs is used to store the value of Cij computed by each thread. 
 
-    //  #pragma unroll
-    //  for(int i = 0; i < 4; i++)
-    //  {
-    //     As[ty+Tw[i]][tx] = 0;
-    //  }
-    //  #pragma unroll
-    //  for(int i = 0; i < 4; i++)
-    //  {
-    //     Bs[ty+Tw[0]][tx] = 0;
-    //  }
+     //Initialising the values of all the values of As and Bs computed by this thread to 0
+
      As[ty][tx] = 0;
      As[ty+Tw[1]][tx] = 0;
      As[ty+Tw[2]][tx] = 0;
@@ -46,6 +38,8 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
      Bs[ty+Tw[6]][tx] = 0;
      Bs[ty+Tw[7]][tx] = 0;
 
+     // Section of the code that handles the cases of when the size of matrix is not a multiple of the block size. 
+
      if(N%TW !=0)
      {
        for(int Ai =I; (Ai < N) && (Ai < I + TW); Ai +=Tw[1])
@@ -62,6 +56,8 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
        }
        __syncthreads();
 
+       //Computing the values of Cs for the corner case.
+
        for(int k = 0; k < TW; k++)
        {
          Cs[0] += As[ty][k] * Bs[k][tx];
@@ -77,18 +73,13 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
      }
       __syncthreads();
 
+    //This section of the code deals with the case of all the blocks that are not the corner cases.   
+
     for(int kk = 0; kk < edge_limit; kk++)
     {
-      // #pragma unroll
-      // for(int i = 0; i < 4; i++)
-      // {
-      //   As[ty+Tw[i]][tx] = A[(I+Tw[i])*N+(kk*TW+tx)];
-      // }
-      // #pragma unroll
-      // for(int i = 0; i < 4; i++)
-      // {
-      //   Bs[ty+Tw[i]][tx] = B[(kk*TW+ty+Tw[i])*N+J];
-      // }
+
+      //Laoding all the values of As and Bs Blocks across threads for the computation.
+
       As[ty][tx] = A[I*N+(kk*TW+tx)];
       As[ty+Tw[1]][tx] = A[(I+Tw[1])*N+(kk*TW+tx)];
       As[ty+Tw[2]][tx] = A[(I+Tw[2])*N+(kk*TW+tx)];
@@ -97,6 +88,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
       As[ty+Tw[5]][tx] = A[(I+Tw[5])*N+(kk*TW+tx)];
       As[ty+Tw[6]][tx] = A[(I+Tw[6])*N+(kk*TW+tx)];
       As[ty+Tw[7]][tx] = A[(I+Tw[7])*N+(kk*TW+tx)];
+      
       Bs[ty][tx] = B[(kk*TW+ty)*N+J];
       Bs[ty+Tw[1]][tx] = B[(kk*TW+ty+Tw[1])*N+J];
       Bs[ty+Tw[2]][tx] = B[(kk*TW+ty+Tw[2])*N+J];
@@ -107,6 +99,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
       Bs[ty+Tw[7]][tx] = B[(kk*TW+ty+Tw[7])*N+J];
       __syncthreads();
 
+      //Computing the values of Cs for the cases of threads blocks that are not in the corner case.
 
       for(int k = 0; k < TW; k++)
       {
@@ -123,17 +116,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
       __syncthreads();
     }
 
-// not helping.
-
-    // for(int Ci = I,csub=0; (Ci<N)&&(Ci<I+TW); Ci += Tw[1],csub += 1) {
-    //   if(J<N) C[Ci*N+J] = Cs[csub];
-    // }
-
-    // for(int Ci = 0; Ci <4 && (I+Tw[1]*Ci) < N; Ci++)
-    //       {
-    //         if(J<N)
-    //         C[(I+Tw[1]*Ci)*N + J] = Cs[Ci];
-    //       }
+    // Storing the values of Cs to C and checking at each stage if the indices are within range or not. 
 
     if((I<N)&&(J<N))
     {
@@ -152,20 +135,5 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
         C[(I+Tw[6])*N+J] = Cs[6];
       if(I+Tw[7] < N)
         C[(I+Tw[7])*N+J] = Cs[7];
-      // if(I+Tw[3] < N) {
-      //   C[I*N+J] = Cs[0];
-      //   C[(I+Tw[1])*N+J] = Cs[1];
-      //   C[(I+Tw[2])*N+J] = Cs[2];
-      //   C[(I+Tw[3])*N+J] = Cs[3];
-      // } else if(I+Tw[2] < N){
-      //   C[I*N+J] = Cs[0];
-      //   C[(I+Tw[1])*N+J] = Cs[1];
-      //   C[(I+Tw[2])*N+J] = Cs[2];
-      // } else if(I+Tw[1] < N) {
-      //   C[I*N+J] = Cs[0];
-      //   C[(I+Tw[1])*N+J] = Cs[1];
-      // } else if(I < N) {
-      //   C[I*N+J] = Cs[0];
-      // }
     }
 }
